1d7c4234c97e69cb26fa7f3e9aa5c3b8717d00e0
Petar2019 <158144521+Petar20051@users.noreply.github.com>
2024-09-04 14:56:15 +0300

    Update towns.js

--- towns.js
+++ towns.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
 });
+$(document).ready(function() { … $('#btnAdd').click(addTown); });
 function addTown() { let townName = $('#townNameForAdd').val(); $('#townNameForAdd').val(''); $('#towns').append($('<option>').text(townName)); $('#result').text(townName + " added."); }
 function deleteTown() {
 	let townName = $('#townName').val();
